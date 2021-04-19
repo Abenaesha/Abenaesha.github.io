0a8c8b2bd6f2fcc92791c97d5a34bf6dd10b418c
abenaesha <abenaesha@github.com>
2021-04-19 18:08:18 +0100

    add gitbiz project

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-// const array = [
-//   { x: 'foo', y: 8 },
-//   { x: 'bar', y: 3 },
-//   { x: 'baz', y: 7 },
-// ];
-
-// const foo = array.reduce((a,b) => ({
-//   ...a,
-//   [b.x]: b.y,
-// }),
-// {}
-// )
-
-// console.log(foo)
-
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
@@ -32,16 +17,16 @@ document.addEventListener('scroll', () => {
 	}
 });
 
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+} );
+
 function scrollTop() {
   const scrollTop = document.getElementById('scroll-top')
   if (this.scrollY >= 500) scrollTop.classList.add('show-scroll');
   else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop)
-
-menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
-	});
-});
